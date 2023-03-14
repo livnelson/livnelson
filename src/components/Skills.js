70c324fa01e3f0f977085193d21a0c408bb9f9e6
Liv Nelson <livnelson@me.com>
2023-03-13 18:30:58 -0700

    bug fixes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,40 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import SkillCard from './SkillCard'
+import React from 'react'
 import '../styles/Skills.css'
 
 function Skills() {
-  const [errors, setErrors] = useState([])
-  const [skills, setSkills] = useState([])
-
-  // useEffect(() => {
-  //   fetch('/skills').then((r) => {
-  //     if (r.ok) {
-  //       r.json().then((skills) => {
-  //         console.log(skills)
-  //         setSkills(skills)
-  //       });
-  //     } else {
-  //       r.json().then((err) => setErrors(err.errors));
-  //     }
-  //   });
-  // }, [])
-
-  // const mappedSkills = skills.map((skill) => {
-  //   return <SkillCard
-  //     key={skill.id}
-  //     id={skill.id}
-  //     skill={skill.skill}
-  //     stars={skill.stars}
-  //     skillLogo={skill.skill_logo}
-  //   />
-  // })
 
   return (
     <div id='skills'>
       <div div className='skills-section' >
         <h2 className='skills-heading'>Skills</h2>
         <div className='skills'>
-          {/* {mappedSkills} */}
           <img className='skill-image' src='https://res.cloudinary.com/dovuffpii/image/upload/v1677632132/skills/javascript_qvsvgu.png' alt='JavaScript' />
           <img className='skill-image' src='https://res.cloudinary.com/dovuffpii/image/upload/v1677632132/skills/react_cfjxcg.png' alt='React' />
           <img className='skill-image' src='https://res.cloudinary.com/dovuffpii/image/upload/v1677632133/skills/ruby_fs8aim.png' alt='Ruby' />
@@ -48,7 +21,6 @@ function Skills() {
           <img className='skill-image' src='https://res.cloudinary.com/dovuffpii/image/upload/v1677632901/skills/solidjs_aec8n3.png' alt='Solid.js' />
           <img className='skill-image' src='https://res.cloudinary.com/dovuffpii/image/upload/v1677632133/skills/woocommerce_ji76cm.png' alt='WooCommerce' />
         </div>
-        {errors ? <div className="errors">{errors}</div> : null}
       </div >
     </div>
   )
